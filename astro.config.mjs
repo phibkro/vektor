c223a97fe84faa8c804b721e4595ce9d4d8186c8
phibkro <71797726+phibkro@users.noreply.github.com>
2024-08-06 03:22:53 +0200

    build: :construction_worker: configure Dockerfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,9 @@ export default defineConfig({
     applyBaseStyles: false
   }), db()],
   site: "https://phibkro.github.io",
-  base: "vektor"
+  base: "vektor",
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
